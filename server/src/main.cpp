e5f28058446183224ead60be04e20d85a5b87019
Guillaume Babin <645219+gbabin@users.noreply.github.com>
2011-12-06 15:57:52 +0100

    full server ui preview

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -17,10 +17,13 @@ int main(int argv, char *argc[])
         m_ping, 
         m_quit
     };
-    
+
+    int preview_ping = 0;
+
+    char command[256];
     int row,col;
     int mode_ch;
-    enum mode_e mode = m_start;
+    enum mode_e mode = m_start, prev_mode;
 
     getmaxyx(stdscr,row,col);
 
@@ -31,33 +34,49 @@ int main(int argv, char *argc[])
         switch (mode)
         {
         case m_start:
+            prev_mode = mode;
             attron(A_BOLD);
-            printw("Welcome\n");
+            mvprintw(2,2,"Welcome");
             attroff(A_BOLD);
-            mvprintw(5,5,"c -> command\n");
-            mvprintw(6,5,"l -> log\n");
-            mvprintw(7,5,"p -> ping\n");
-            mvprintw(8,5,"q -> quit\n");
-            break;
+            mvprintw(5,5,"c -> command");
+            mvprintw(6,5,"l -> log");
+            mvprintw(7,5,"p -> ping");
+            mvprintw(8,5,"q -> quit");
+            move(row-1,col-1);
+            break; 
         case m_command:
-            printw("Command mode\n");
-            printw("> ");
+            nocbreak();
+            echo();
+            mvprintw(2,2,"Command mode");
+            mvprintw(3,2,">");
+            move(3,4);
+            getstr(command);
+            mvprintw(10,3,"Execution of ...");
+            mvprintw(11,3,"%s", command);
+            mode = prev_mode;
             break;
         case m_log:
-            printw("Log mode\n");
+            prev_mode = mode;
+            mvprintw(2,2,"Log mode");
+            for (int i=4 ; i < row-2 ; i++)
+                mvprintw(i,3,"[%2d] Message", i-3);
+            move(row-1,col-1);
             break;
         case m_ping:
-            printw("Ping mode\n");
+            prev_mode = mode;
+            mvprintw(2,2,"Ping mode");
+            mvprintw(5,2,"Client : %d ms", preview_ping++);
+            move(row-1,col-1);
             break;
         default:
             break;
         }
 
-        move(row-1,col-1);
-
         refresh();
+        
+        halfdelay(10);
 
-        mode_ch = getch(); // halfdelay()
+        mode_ch = getch();
 
         switch (mode_ch) {
         case 'c':
@@ -75,6 +94,8 @@ int main(int argv, char *argc[])
         default:
             break;
         }
+
+        mode_ch = 0;
     }
     
     endwin(); // end curses mode
