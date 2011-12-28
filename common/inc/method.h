7ac2747475463dfb7161b88f99f0de764e1fc996
Rémi Palandri <palandrir@gmail.com>
2011-12-28 01:25:40 +0100

    added method template that provides an overlay to all the class methods (to pass them as arguments)

---
+++ common/inc/method.h
@@ -0,0 +1,23 @@
+#ifndef DEF_METHOD
+#define DEF_METHOD
+
+// constructor is called  : Method<C> met(instance,&C::func_name)
+
+template <class C>
+class Method {
+public:
+    Method(C &ptr,void (C::*met)(void)) :  ptr(ptr), met(met)  {
+        
+    }
+    void call() {
+        (ptr.*met)();
+    }
+    
+private:
+    C &ptr;
+    void (C::*met)(void);
+};
+
+#endif
+
+
