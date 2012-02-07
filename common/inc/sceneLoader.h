ec79d8ac250ffcd2430b8f14e0c8c066a79abc84
Guillaume Babin <645219+gbabin@users.noreply.github.com>
2012-02-07 15:20:41 +0100

    XML parser started

---
+++ common/inc/sceneLoader.h
@@ -0,0 +1,12 @@
+#ifndef DEF_SCENELOADER
+#define DEF_SCENELOADER
+
+#include <string>
+#include <tinyxml.h>
+
+class SceneLoader {
+public:
+  Scene load(String nom);
+}
+
+#endif
---
+++ common/src/sceneLoader.cpp
@@ -0,0 +1,7 @@
+#include "sceneLoader.h"
+
+using namespace std;
+
+Scene SceneLoader::load(String nom) {
+
+}
