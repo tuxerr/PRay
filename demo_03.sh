17f5812ee78d0df64fb12fab7d003c767ef77d2a
Guillaume Babin <645219+gbabin@users.noreply.github.com>
2016-02-06 00:50:36 +0100

    update demo 3

--- demo_03.sh
+++ demo_03.sh
@@ -1,36 +1,20 @@
 #!/usr/bin/env bash
 
 PORT=12345
+NB_CLIENTS=4
 
 cmake .
 
 make
 
-(
-    cd client
-    sleep 2
-    ./pray_client --server 127.0.0.1 --port $PORT > /dev/null
-)&
+cd client
+for i in $(seq $NB_CLIENTS)
+do
+    ./pray_client --server 127.0.0.1 --port $PORT --fork > pray_client.$i.out.log 2> pray_client.$i.err.log
+done
 
-(
-    cd client
-    sleep 3
-    ./pray_client --server 127.0.0.1 --port $PORT > /dev/null
-)&
-
-(
-    cd client
-    sleep 4
-    ./pray_client --server 127.0.0.1 --port $PORT > /dev/null
-)&
-
-(
-    cd client
-    sleep 5
-    ./pray_client --server 127.0.0.1 --port $PORT > /dev/null
-)&
-
-cd server
+cd ../server
 ./pray_server --port $PORT
-# type: CALCULATE ../scenes/bille.xml
-killall pray_client
+
+#type: scene ../scenes/bille.xml
+#type: render
