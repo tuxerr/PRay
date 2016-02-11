54d84d07c90f1f2e47cad73d305b2bb80b9612dc
Guillaume Babin <645219+gbabin@users.noreply.github.com>
2016-02-11 20:18:06 +0100

    modified generateBackground.py & added new rendered images

--- scenes/generateBackground.py
+++ scenes/generateBackground.py
@@ -1,4 +1,5 @@
-# -*-coding:Utf-8 -* 
+#!/usr/bin/env python2
+# -*-coding:Utf-8 -*
 
 # Usage : python generateBackground.py > testRefraction.xml
 
@@ -27,16 +28,18 @@ def genTriangle(a, b, c, col):
 
 def generateBackground(size, number):
     result = ''
+    base = 200
+    offset = 30
     for i in range(number):
         for j in range(number):
-            result += genTriangle([i*size, j*size, 0], [(i+1)*size, (j+1)*size, 0], [i*size, (j+1)*size, 0], [255*((i+j)%2), 255*((i+j)%2), 255*((i+j)%2)])
-            result += genTriangle([i*size, j*size, 0], [(i+1)*size, j*size, 0], [(i+1)*size, (j+1)*size, 0], [255*((i+j)%2), 255*((i+j)%2), 255*((i+j)%2)])
+            result += genTriangle([i*size, j*size, 0], [(i+1)*size, (j+1)*size, 0], [i*size, (j+1)*size, 0], [offset+base*((i+j)%2), offset+base*((i+j)%2), offset+base*((i+j)%2)])
+            result += genTriangle([i*size, j*size, 0], [(i+1)*size, j*size, 0], [(i+1)*size, (j+1)*size, 0], [offset+base*((i+j)%2), offset+base*((i+j)%2), offset+base*((i+j)%2)])
 
-            result += genTriangle([0, i*size, j*size], [0, (i+1)*size, (j+1)*size], [0, i*size, (j+1)*size], [255*((i+j)%2), 255*((i+j)%2), 255*((i+j)%2)])
-            result += genTriangle([0, i*size, j*size], [0, (i+1)*size, j*size], [0, (i+1)*size, (j+1)*size], [255*((i+j)%2), 255*((i+j)%2), 255*((i+j)%2)])
+            result += genTriangle([0, i*size, j*size], [0, (i+1)*size, (j+1)*size], [0, i*size, (j+1)*size], [offset+base*((i+j)%2), offset+base*((i+j)%2), offset+base*((i+j)%2)])
+            result += genTriangle([0, i*size, j*size], [0, (i+1)*size, j*size], [0, (i+1)*size, (j+1)*size], [offset+base*((i+j)%2), offset+base*((i+j)%2), offset+base*((i+j)%2)])
 
-            result += genTriangle([i*size, 0, j*size], [i*size, 0, (j+1)*size], [(i+1)*size, 0, (j+1)*size], [255*((i+j)%2), 255*((i+j)%2), 255*((i+j)%2)])
-            result += genTriangle([i*size, 0, j*size], [(i+1)*size, 0, (j+1)*size], [(i+1)*size, 0, j*size], [255*((i+j)%2), 255*((i+j)%2), 255*((i+j)%2)])
+            result += genTriangle([i*size, 0, j*size], [i*size, 0, (j+1)*size], [(i+1)*size, 0, (j+1)*size], [offset+base*((i+j)%2), offset+base*((i+j)%2), offset+base*((i+j)%2)])
+            result += genTriangle([i*size, 0, j*size], [(i+1)*size, 0, (j+1)*size], [(i+1)*size, 0, j*size], [offset+base*((i+j)%2), offset+base*((i+j)%2), offset+base*((i+j)%2)])
     return result
 
 
@@ -46,8 +49,8 @@ def generateRefractionScene():
     result = '<?xml version="1.0" encoding="utf-8"?>\n'
     result+= '<scene>\n'
     result+= '\t<camera>\n'
-    result+= '\t\t<position x="4" y="3" z="2"/>\n'
-    result+= '\t\t<target x="1" y="1" z="1"/>\n'
+    result+= '\t\t<position x="7" y="5" z="2.5"/>\n'
+    result+= '\t\t<target x="1" y="1" z="0.75"/>\n'
     result+= '\t\t<normal x="0" y="0" z="1"/>\n'
     result+= '\t\t<viewplane w="16/2" h="9/2" d="35"/>\n'
     result+= '\t</camera>\n'
@@ -60,26 +63,64 @@ def generateRefractionScene():
     result+= '\t<ambientLight>\n'
     result+= '\t\t<color r="255" g="255" b="255"/>\n'
     result+= '\t</ambientLight>\n'
-    
+
     result+= '\t<object>\n'
     result+= '\t\t<shape>\n'
     result+= '\t\t\t<sphere>\n'
-    result+= '\t\t\t\t<center x="1" y="1" z="1"/>\n'
-    result+= '\t\t\t\t<radius v="0.5"/>\n'
+    result+= '\t\t\t\t<center x="2" y="2" z="0.9"/>\n'
+    result+= '\t\t\t\t<radius v="0.3"/>\n'
     result+= '\t\t\t</sphere>\n'
     result+= '\t\t</shape>\n'
     result+= '\t\t<material>\n'
     result+= '\t\t\t<phong>\n'
-    result+= '\t\t\t\t<color r="50" g="50" b="255"/>\n'
+    result+= '\t\t\t\t<color r="220" g="30" b="30"/>\n'
+    result+= '\t\t\t\t<specular v="0.4"/>\n'
+    result+= '\t\t\t\t<diffuse v="0.6"/>\n'
+    result+= '\t\t\t\t<ambiant v="0.05"/>\n'
+    result+= '\t\t\t\t<shininess v="100"/>\n'
+    result+= '\t\t\t\t<reflexivity v="0.3"/>\n'
+    result+= '\t\t\t</phong>\n'
+    result+= '\t\t</material>\n'
+    result+= '\t</object>\n'
+
+    result+= '\t<object>\n'
+    result+= '\t\t<shape>\n'
+    result+= '\t\t\t<sphere>\n'
+    result+= '\t\t\t\t<center x="1.5" y="1" z="1"/>\n'
+    result+= '\t\t\t\t<radius v="0.4"/>\n'
+    result+= '\t\t\t</sphere>\n'
+    result+= '\t\t</shape>\n'
+    result+= '\t\t<material>\n'
+    result+= '\t\t\t<phong>\n'
+    result+= '\t\t\t\t<color r="50" g="50" b="240"/>\n'
     result+= '\t\t\t\t<specular v="0.6"/>\n'
     result+= '\t\t\t\t<diffuse v="0.5"/>\n'
     result+= '\t\t\t\t<ambiant v="0.05"/>\n'
     result+= '\t\t\t\t<shininess v="100"/>\n'
-    result+= '\t\t\t\t<reflexivity v="1"/>\n'
+    result+= '\t\t\t\t<reflexivity v="0.3"/>\n'
     result+= '\t\t\t</phong>\n'
     result+= '\t\t</material>\n'
     result+= '\t</object>\n'
-    
+
+    result+= '\t<object>\n'
+    result+= '\t\t<shape>\n'
+    result+= '\t\t\t<sphere>\n'
+    result+= '\t\t\t\t<center x="1.5" y="1" z="1.5"/>\n'
+    result+= '\t\t\t\t<radius v="0.2"/>\n'
+    result+= '\t\t\t</sphere>\n'
+    result+= '\t\t</shape>\n'
+    result+= '\t\t<material>\n'
+    result+= '\t\t\t<phong>\n'
+    result+= '\t\t\t\t<color r="50" g="50" b="240"/>\n'
+    result+= '\t\t\t\t<specular v="0.6"/>\n'
+    result+= '\t\t\t\t<diffuse v="0.5"/>\n'
+    result+= '\t\t\t\t<ambiant v="0.05"/>\n'
+    result+= '\t\t\t\t<shininess v="100"/>\n'
+    result+= '\t\t\t\t<reflexivity v="0.3"/>\n'
+    result+= '\t\t\t</phong>\n'
+    result+= '\t\t</material>\n'
+    result+= '\t</object>\n'
+
     result += generateBackground(0.5, 10)
     result += '</scene>\n'
     print(result)
